892deaffb45778263b17c0d60967ad235ef108a7
mathlete <jr.mathlete@gmail.com>
2022-06-15 13:37:51 -0600

    Added database layer stubs for user objects

---
+++ database/user.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"fmt"
+)
+
+// Function stubs for when the model specifics are decided on
+
+func AddUser() {
+	fmt.Println("Hello World")
+	return
+}
+
+func DeleteUser() {
+	return
+}
+
+func UpdateUser() {
+	return
+}
+
+func GetUserFromUsername(username string) {
+	return
+}
+
+func GetUserFromID(id uint) {
+	return
+}
+
+func AddCredentialToUser(username string /*Credential Object*/) {
+	return
+}
