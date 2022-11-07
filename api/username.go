49e97f43adc159c7f410c7b5dd6fa1a6d9220d41
10808249 <jhjun98@gmail.com>
2022-11-07 13:26:07 -0700

    fixing to use current code instead of legacy

--- api/username.go
+++ api/username.go
@@ -1,19 +1,17 @@
 package api
 
 import (
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
+	"github.com/Usable-Security-and-Privacy-Lab/lets-auth-ca/certs"
+	"github.com/Usable-Security-and-Privacy-Lab/lets-auth-ca/legacy/certificates"
+	"github.com/Usable-Security-and-Privacy-Lab/lets-auth-ca/models"
 	"github.com/gorilla/mux"
-
-	"github.com/Usable-Security-and-Privacy-Lab/lets-auth-ca/models/certificates"
-)
-
-var (
-	serviceCertificateValidDays       = "10"
-	authenticatorCertificateValidDays = "7"
 )
 
 // certificateRequest represents the CSR for a single certificate
@@ -43,12 +41,82 @@ func ObtainNewCertificate(w http.ResponseWriter, r *http.Request) {
 	// Verify Authenticator Signature
 	authCert, CSR, signature := request.AuthenticatorCertificate, request.CSR, request.AuthSignature
 
+	user, err := models.GetUserByUsername(username)
+	if err != nil {
+		// user isn't in database
+		fmt.Printf("User is not in database")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	// Sign the Certificate
-	certByte := []byte(CSR)
-	signedCert, ok := certificates.SignCert(certByte, authCert, serviceCertificateValidDays, true)
+	cert, _ := pem.Decode([]byte(CSR))
+	var csr *x509.CertificateRequest
+
+	// var err error
+	if cert == nil {
+		csr, err = x509.ParseCertificateRequest([]byte(CSR))
+		// checkError(err)
+	} else {
+		csr, err = x509.ParseCertificateRequest(cert.Bytes)
+		// checkError(err)
+	}
+	if err != nil {
+		fmt.Println("CSR bad format", err.Error())
+		jsonResponse(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Check that the CSR is for one of the valid authenticator public keys
+	// First, get the authorized keys for this user
+	authKeys, err := models.GetAuthKeysForUser(user)
+	if err != nil {
+		jsonResponse(w, "unable to get authenticator keys for this user", http.StatusInternalServerError)
+		return
+	}
+	// Second, convert the public key in the CSR into PEM format
+	publicKeyDer, _ := x509.MarshalPKIXPublicKey(csr.PublicKey)
+	publicKeyBlock := pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyDer,
+	}
+	publicKey := string(pem.EncodeToMemory(&publicKeyBlock))
+
+	// Third, check if the key matches any of the valid keys for this user
+	present := models.AuthKeyPresent(publicKey, authKeys)
+	if !present {
+		jsonResponse(w, "authenticator key is not authorized for this account", http.StatusUnauthorized)
+		return
+	}
+
+	// TBD we need to validate the CSR. This should include being sure it is properly signed.
+	// It is for the username this account owns.
+	// What else?
+	if username != csr.Subject.CommonName {
+		jsonResponse(w, "username doesn't match CSR subject", http.StatusBadRequest)
+		return
+	}
+
+	signedCert, err := certs.SignAuthCertificate(csr)
+	pemCert := string(pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: signedCert.Raw}))
+
+	if err != nil {
+		switch {
+		case pemCert == "Invalid Device Cert":
+			w.WriteHeader(403)
+			fmt.Fprint(w, pemCert)
+			fmt.Println("403, Invalid Device Cert")
+			// Do these need returns? ask if the old Login endpoint was exhaustively tested
+		default:
+			w.WriteHeader(500)
+			fmt.Fprint(w, pemCert)
+			fmt.Printf("500, %s\n", pemCert)
+		}
+		return
+	}
 
 	// Verify that accountID isn't taken
-	err := certificates.VerifyAccountIDTaken(signedCert, username)
+	err = certificates.VerifyAccountIDTaken(pemCert, username)
 	if err != nil {
 		if err.Error() == "account already taken by other user" {
 			w.WriteHeader(403)
@@ -59,7 +127,7 @@ func ObtainNewCertificate(w http.ResponseWriter, r *http.Request) {
 
 	// TODO: Verify if the accountID of user already exists (check not to put twice)
 	// maybe it can be combined with above statements
-	err = certificates.VerifyAccountIDExists(signedCert, username)
+	err = certificates.VerifyAccountIDExists(pemCert, username)
 	if err != nil {
 		if err.Error() == "account already exists by current user" {
 			w.WriteHeader(403)
@@ -75,31 +143,15 @@ func ObtainNewCertificate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if !ok {
-		// AGAIN ASK FOR CLERIFICATION
-		switch {
-		case signedCert == "Invalid Device Cert":
-			w.WriteHeader(403)
-			fmt.Fprint(w, signedCert)
-			fmt.Println("403, Invalid Device Cert")
-			// Do these need returns? ask if the old Login endpoint was exhaustively tested
-		default:
-			w.WriteHeader(500)
-			fmt.Fprint(w, signedCert)
-			fmt.Printf("500, %s\n", signedCert)
-		}
-		return
-	}
-
 	// Add the certificate to the database
-	err = certificates.AddCert(signedCert, authCert)
+	err = certificates.AddCert(pemCert, authCert)
 	if err != nil {
 		// AGAIN ASK FOR CLERIFICATION
 		jsonResponse(w, fmt.Errorf("failed adding cert to certs table"), http.StatusInternalServerError)
 		return
 	}
 
-	response := serviceResponse{SignedCertificate: signedCert}
+	response := serviceResponse{SignedCertificate: pemCert}
 	final, _ := json.Marshal(response)
 
 	fmt.Fprint(w, string(final))
